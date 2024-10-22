f3b786a09c214dab3487e299a9b9debf1e9a39d0
Viktoria <v.s.shuvaeva@gmail.com>
2024-10-22 14:27:55 +0700

    fix_home

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -5,6 +5,7 @@ from .request_log_routes import request_logs_ns
 from .schedule_action_routes import schedule_actions_ns
 from .schedule_details_routes import schedule_details_ns
 from .log_route import log_ns
+from .home_route import home_ns
 
 def register_routes(api: Api):
     api.add_namespace(schedules_ns)
@@ -13,3 +14,4 @@ def register_routes(api: Api):
     api.add_namespace(request_logs_ns)
     api.add_namespace(login_ns)
     api.add_namespace(log_ns)
+    api.add_namespace(home_ns)
