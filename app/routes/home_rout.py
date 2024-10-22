756d2e7cc99e70439dee92331b66ab05a05a4869
Viktoria <v.s.shuvaeva@gmail.com>
2024-10-22 13:02:07 +0700

    fix

--- app/routes/home_rout.py
+++ app/routes/home_rout.py
@@ -6,4 +6,4 @@ home_ns = Namespace('', description='Check running')
 @home_ns.route('/')
 class Home(Resource):
     def get(self):
-        return {"message": "The service is running", "hello": "no error"}
+        return {"message": "The service is running", "Hello,": "world!"}
