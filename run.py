c1af0d9f828afd35ed36680b8ad49800b3aae4e3
Viktoria <v.s.shuvaeva@gmail.com>
2024-10-31 13:22:55 +0700

    fix_proxy

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from app import create_app
 
 app = create_app()
 
-
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 if __name__ == '__main__':
     app.run( host='0.0.0.0', port=5000)
